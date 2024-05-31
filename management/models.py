3bc747a73c91458dfd792ce9671d95e11718a0e2
filipwroblewski <filip.wroblewski.row@gmail.com>
2024-05-31 12:10:52 +0200

    add validators to Measurement

--- management/models.py
+++ management/models.py
@@ -1,5 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
+PH_RANGE = (0, 14)
+TEMP_RANGE = (0, 100)
+TDS_RANGE = (0, 2000)
+
+def validate_range(
+        value: float | int, 
+        value_range: tuple[float | int, float | int]) -> None:
+    
+    if len(value_range) != 2:
+        raise ValueError("Invalid range: must be a tuple of exactly two values (min, max).")
+    
+    min_value, max_value = value_range
+    if not isinstance(min_value, (int, float)) or not isinstance(max_value, (int, float)):
+        raise ValueError("Invalid range values: must be integers or floats.")
+
+    if min_value > max_value:
+        raise ValueError("Invalid range: min value cannot be greater than max value.")
+    
+    if not (min_value <= value <= max_value):
+        raise ValidationError(f'Value {value} is out of the valid range (from {min_value} to {max_value}).')
+
 
 class HydroponicSystem(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='hydroponic_systems')
@@ -13,9 +36,18 @@ class HydroponicSystem(models.Model):
 
 class Measurement(models.Model):
     system = models.ForeignKey(HydroponicSystem, on_delete=models.CASCADE, related_name='measurements')
-    ph = models.FloatField()
-    temperature = models.FloatField()
-    tds = models.FloatField()  # Total Dissolved Solids
+    ph = models.FloatField(
+        validators=[lambda value: validate_range(value, PH_RANGE)], 
+        null=True, 
+        blank=True)
+    temperature = models.FloatField(
+        validators=[lambda value: validate_range(value, TEMP_RANGE)], 
+        null=True, 
+        blank=True)
+    tds = models.FloatField(
+        validators=[lambda value: validate_range(value, TDS_RANGE)], 
+        null=True, 
+        blank=True)  # Total Dissolved Solids
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
