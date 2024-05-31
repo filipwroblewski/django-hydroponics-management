d49a2fb240ea96a3da7c17ad3a75a1fd0516b488
filipwroblewski <filip.wroblewski.row@gmail.com>
2024-05-31 13:24:14 +0200

    remove lambdas functions (can't be serialized for migrations) and make more readable measurement timestamp

--- management/models.py
+++ management/models.py
@@ -6,23 +6,21 @@ PH_RANGE = (0, 14)
 TEMP_RANGE = (0, 100)
 TDS_RANGE = (0, 2000)
 
-def validate_range(
-        value: float | int, 
-        value_range: tuple[float | int, float | int]) -> None:
-    
-    if len(value_range) != 2:
-        raise ValueError("Invalid range: must be a tuple of exactly two values (min, max).")
-    
-    min_value, max_value = value_range
-    if not isinstance(min_value, (int, float)) or not isinstance(max_value, (int, float)):
-        raise ValueError("Invalid range values: must be integers or floats.")
+def validate_ph_range(value):
+    validate_range(value, PH_RANGE)
 
-    if min_value > max_value:
-        raise ValueError("Invalid range: min value cannot be greater than max value.")
-    
+def validate_temp_range(value):
+    validate_range(value, TEMP_RANGE)
+
+def validate_tds_range(value):
+    validate_range(value, TDS_RANGE)
+
+def validate_range(value, value_range):
+    min_value, max_value = value_range
+    if not isinstance(value, (int, float)):
+        raise ValueError("Invalid value: must be an integer or float.")
     if not (min_value <= value <= max_value):
         raise ValidationError(f'Value {value} is out of the valid range (from {min_value} to {max_value}).')
-
 
 class HydroponicSystem(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='hydroponic_systems')
@@ -36,19 +34,11 @@ class HydroponicSystem(models.Model):
 
 class Measurement(models.Model):
     system = models.ForeignKey(HydroponicSystem, on_delete=models.CASCADE, related_name='measurements')
-    ph = models.FloatField(
-        validators=[lambda value: validate_range(value, PH_RANGE)], 
-        null=True, 
-        blank=True)
-    temperature = models.FloatField(
-        validators=[lambda value: validate_range(value, TEMP_RANGE)], 
-        null=True, 
-        blank=True)
-    tds = models.FloatField(
-        validators=[lambda value: validate_range(value, TDS_RANGE)], 
-        null=True, 
-        blank=True)  # Total Dissolved Solids
+    ph = models.FloatField(validators=[validate_ph_range], null=True, blank=True)
+    temperature = models.FloatField(validators=[validate_temp_range], null=True, blank=True)
+    tds = models.FloatField(validators=[validate_tds_range], null=True, blank=True)  # Total Dissolved Solids
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Measurement for {self.system.name} at {self.timestamp}"
+        formatted_timestamp = self.timestamp.strftime("%Y-%m-%d %H:%M:%S")
+        return f"Measurement for {self.system.name} at {formatted_timestamp}"
