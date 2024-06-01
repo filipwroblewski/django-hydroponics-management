6a4633708f44be6b286f0c518be8a591a2133f06
filipwroblewski <filip.wroblewski.row@gmail.com>
2024-06-01 14:53:06 +0200

    set data fields as read only and enable filter option in admin panel

--- management/admin.py
+++ management/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import HydroponicSystem, Measurement
 
-admin.site.register(HydroponicSystem)
-admin.site.register(Measurement)
+        
+class HydroponicSystemAdmin(admin.ModelAdmin):
+    list_display = ['id', 'owner', 'name', 'description', 'created_at', 'updated_at']
+    readonly_fields = ['created_at', 'updated_at']
+    list_filter = ['owner', ('created_at', admin.DateFieldListFilter), ('updated_at', admin.DateFieldListFilter)]
+
+class MeasurementAdmin(admin.ModelAdmin):
+    list_display = ['id', 'system', 'ph', 'temperature', 'tds', 'timestamp']
+    readonly_fields = ['timestamp']
+    list_filter = ['system', ('timestamp', admin.DateFieldListFilter)]
+
+admin.site.register(HydroponicSystem, HydroponicSystemAdmin)
+admin.site.register(Measurement, MeasurementAdmin)
--- management/serializers.py
+++ management/serializers.py
@@ -3,6 +3,8 @@ from .models import HydroponicSystem, Measurement
 
 class HydroponicSystemSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    created_at = serializers.DateTimeField(read_only=True)
+    updated_at = serializers.DateTimeField(read_only=True)
 
     class Meta:
         model = HydroponicSystem
@@ -10,6 +12,7 @@ class HydroponicSystemSerializer(serializers.ModelSerializer):
 
 class MeasurementSerializer(serializers.ModelSerializer):
     system = serializers.PrimaryKeyRelatedField(queryset=HydroponicSystem.objects.all())
+    timestamp = serializers.DateTimeField(read_only=True)
 
     class Meta:
         model = Measurement
