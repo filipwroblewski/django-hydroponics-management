ccf2566d5afd5a82ab7dba327b5ffd82624db7b2
filipwroblewski <filip.wroblewski.row@gmail.com>
2024-06-02 18:47:25 +0200

    enable display last 10 measurements, and last X

--- management/views.py
+++ management/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets, permissions, filters
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .models import HydroponicSystem, Measurement
 from .serializers import HydroponicSystemSerializer, MeasurementSerializer
 from .filters import HydroponicSystemFilter, MeasurementFilter
@@ -56,3 +58,21 @@ class MeasurementViewSet(viewsets.ModelViewSet):
         instance = self.get_object()
         check_owner_permission(self.request.user, instance.system.owner)
         serializer.save()
+    
+    @action(detail=False, methods=['get'], url_path='last-measurements')
+    def last_measurements(self, request):
+        system_name = request.query_params.get('system_name')
+        num_measurements = request.query_params.get('num_measurements', 10)
+        
+        try:
+            num_measurements = int(num_measurements)
+        except ValueError:
+            return Response({"error": "num_measurements must be an integer."}, status=400)
+        
+        system = HydroponicSystem.objects.filter(name=system_name, owner=request.user).first()
+        if not system:
+            return Response({"error": "System not found or you do not have permission."}, status=404)
+        
+        measurements = Measurement.objects.filter(system=system).order_by('-timestamp')[:num_measurements]
+        serializer = MeasurementSerializer(measurements, many=True)
+        return Response(serializer.data)
