3b186c0be18ea1e6db16836e1c9ac1704c50631a
filipwroblewski <filip.wroblewski.row@gmail.com>
2024-06-03 11:08:19 +0200

    create hydroponic system partial update test case

--- management/tests/test_hydroponic_system.py
+++ management/tests/test_hydroponic_system.py
@@ -127,3 +127,69 @@ class HydroponicSystemUpdateTestCase(APITestCase):
         self.assertEqual(response.data['detail'], 'Given token not valid for any token type')
 
 
+class HydroponicSystemPartialUpdateTestCase(APITestCase):
+
+    def generate_jwt_token(self, username, password):
+        # Function to generate JWT token
+        url = '/api/token/'
+        response = self.client.post(url, {'username': username, 'password': password}, format='json')
+        if response.status_code == 200:
+            return response.json()['access']
+        return None
+
+    def setUp(self):
+        # Clear existing data
+        HydroponicSystem.objects.all().delete()
+        User.objects.all().delete()
+
+        # Create a test user
+        self.user = User.objects.create_user(username='test_user', password='test_password')
+
+        # Create a hydroponic system for the user
+        self.system = HydroponicSystem.objects.create(owner=self.user, name='Test System', description='Test Description')
+
+        # Generate JWT token for the user
+        self.access_token = self.generate_jwt_token('test_user', 'test_password')
+
+    def test_hydroponic_system_partial_update_name_authenticated(self):
+        # Make an authenticated request to update just the name of the hydroponic system
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.access_token)
+        url = f'/api/hydroponic-systems/{self.system.id}/'
+        data = {'name': 'Test System - Name Update'}
+        response = self.client.patch(url, data, format='json')
+        self.assertEqual(response.status_code, 200)
+
+        # Check if the name was updated
+        updated_system = HydroponicSystem.objects.get(id=self.system.id)
+        self.assertEqual(updated_system.name, 'Test System - Name Update')
+        self.assertEqual(updated_system.description, 'Test Description')
+
+    def test_hydroponic_system_partial_update_description_authenticated(self):
+        # Make an authenticated request to update just the description of the hydroponic system
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.access_token)
+        url = f'/api/hydroponic-systems/{self.system.id}/'
+        data = {'description': 'Updated description'}
+        response = self.client.patch(url, data, format='json')
+        self.assertEqual(response.status_code, 200)
+
+        # Check if the description was updated
+        updated_system = HydroponicSystem.objects.get(id=self.system.id)
+        self.assertEqual(updated_system.name, 'Test System')
+        self.assertEqual(updated_system.description, 'Updated description')
+
+    def test_hydroponic_system_partial_update_unauthenticated(self):
+        # Make an unauthenticated request to update just the name of the hydroponic system
+        url = f'/api/hydroponic-systems/{self.system.id}/'
+        data = {'name': 'Test System - Name Update'}
+        response = self.client.patch(url, data, format='json')
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.data['detail'], 'Authentication credentials were not provided.')
+
+    def test_hydroponic_system_partial_update_invalid_token(self):
+        # Make a request with an invalid token to update just the name of the hydroponic system
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + 'invalid_token')
+        url = f'/api/hydroponic-systems/{self.system.id}/'
+        data = {'name': 'Test System - Name Update'}
+        response = self.client.patch(url, data, format='json')
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.data['detail'], 'Given token not valid for any token type')
