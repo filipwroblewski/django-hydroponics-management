7643b95ec829a1ad176d4d2cfbd30caf076a1b8a
filipwroblewski <filip.wroblewski.row@gmail.com>
2024-06-03 13:17:34 +0200

    Improve filters readability

--- management/filters.py
+++ management/filters.py
@@ -10,10 +10,16 @@ class HydroponicSystemFilter(django_filters.FilterSet):
         fields = ['created_at', 'updated_at']
 
 class MeasurementFilter(django_filters.FilterSet):
+    """
+    Filter class which provides (min/max) range filters for pH, temperature, and TDS values.
+    """
+    
     ph_min = django_filters.NumberFilter(field_name='ph', lookup_expr='gte')
     ph_max = django_filters.NumberFilter(field_name='ph', lookup_expr='lte')
+
     temperature_min = django_filters.NumberFilter(field_name='temperature', lookup_expr='gte')
     temperature_max = django_filters.NumberFilter(field_name='temperature', lookup_expr='lte')
+    
     tds_min = django_filters.NumberFilter(field_name='tds', lookup_expr='gte')
     tds_max = django_filters.NumberFilter(field_name='tds', lookup_expr='lte')
 
